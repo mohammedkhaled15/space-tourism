1cc5a7cf4e00ccfc587b487fa14d926951a1bed5
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2022-01-12 01:42:04 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+// close and open of menu in mobile screen
+
 const openMark = document.querySelector(".menu-btn")
 const mainMenu = document.querySelector(".side-menu")
 const closeMark = document.querySelector(".side-menu img")
 
 openMark.addEventListener("click", function(){
-    mainMenu.classList.remove("disappear")
+    mainMenu.style.cssText = "display:block; visibility: visible; opacity:1"
 })
 closeMark.addEventListener("click", function(){
-    mainMenu.classList.add("disappear")
+    mainMenu.style.cssText = "display:none; visibility: hidden; opacity:0"
+})
+
+/*************************************************************************************************************/
+
+// moving between pages and adding class Active
+
+let linkItems = document.querySelectorAll(".side-menu ul a")
+
+linkItems.forEach((item)=>{
+    item.addEventListener("click",function(){
+        linkItems.forEach((el)=>el.classList.remove("active"))
+        item.classList.add("active")
+    })
 })
