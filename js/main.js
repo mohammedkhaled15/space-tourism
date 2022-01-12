252d06d4c5526e873425a30dc8bd8a62cb1dd34b
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2022-01-12 18:41:25 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ linkItems.forEach((item)=>{
         item.classList.add("active")
     })
 })
+
+/*****************************************************************************************************************/
+
+// image slider 
+
+let imgs = document.querySelectorAll(".destination .img-slider img")
+let btns = document.querySelectorAll(".destination .dest-menu a")
+let infos = document.querySelectorAll(".destination .info-slider .card")
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click", function(){
+        btns.forEach((btn)=>btn.classList.remove("active")) //removing class active first from all links
+        btn.classList.add("active") // adding class active to the clicked link
+
+        // Manipulating with img slider
+        imgs.forEach((img)=>{
+
+            img.classList.remove("visible") // removing class visible from all images first
+
+            // condition if the source of image contains the data-class of the button then add class cisible to that image
+            if(img.getAttribute("src").indexOf(btn.dataset.class)!== -1){ 
+                img.classList.add("visible")
+            }
+        })
+
+        // Manipulating with infos slider
+        infos.forEach((info)=>{
+            info.classList.remove("visible")
+            if(info.classList.contains(btn.dataset.class)){
+                info.classList.add("visible")
+            }
+        })
+    })
+})
