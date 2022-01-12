6c9e59c8c3eadee6898f2a6c778f415df2004e9d
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2022-01-12 23:42:32 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -24,12 +24,16 @@ closeMark.addEventListener("click", function(){
 
 // moving between pages and adding class Active
 
-let linkItems = document.querySelectorAll(".side-menu ul a")
+const linkItems = document.querySelectorAll(".side-menu ul a")
 
 linkItems.forEach((item)=>{
     item.addEventListener("click",function(){
         linkItems.forEach((el)=>el.classList.remove("active"))
         item.classList.add("active")
+
+        if(window.innerWidth <= 575){
+            mainMenu.style.cssText = "display:none; visibility: hidden; opacity:0"
+        }
     })
 })
 
@@ -37,9 +41,9 @@ linkItems.forEach((item)=>{
 
 // image slider 
 
-let imgs = document.querySelectorAll(".destination .img-slider img")
-let btns = document.querySelectorAll(".destination .dest-menu a")
-let infos = document.querySelectorAll(".destination .info-slider .card")
+const imgs = document.querySelectorAll(".destination .img-slider img")
+const btns = document.querySelectorAll(".destination .dest-menu a")
+const infos = document.querySelectorAll(".destination .info-slider .card")
 
 btns.forEach((btn)=>{
     btn.addEventListener("click", function(){
@@ -59,8 +63,12 @@ btns.forEach((btn)=>{
 
         // Manipulating with infos slider
         infos.forEach((info)=>{
-            info.classList.remove("visible")
+            info.classList.remove("visible") // first removing class visible from all info
+
+            // Check if info classes has class named with the dataset class name of the btn
             if(info.classList.contains(btn.dataset.class)){
+
+                // Adding visible class tho that info
                 info.classList.add("visible")
             }
         })
